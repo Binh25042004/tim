14a7ac4400a4c7414d130fd9b76d463c614065f7
BINH25042004 <binhnq2504@gmail.com>
2025-03-31 21:21:12 +0700

    luu1

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất nó vốn lạ thường mà sao bé cứ đi nhầm đường vào tim tôi`,
+    introTitle: 'Cô em à!',
+    introDesc: ``,
     btnIntro: 'click ik há há',
-    title: 'Phải chăng bé đã thích tôi từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng bé đã thích tôi từ cái nhìn đầu tiên 😙',
+    title: 'Mình như vậy có được gọi là iu chưa ta , cô em có thích toai không nhờ 😙',
+    desc: 'Mình như vậy có được gọi là iu chưa ta , cô em có thích toai không nhờ 😙',
     btnYes: 'Thích mà ngại <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao mình vẫn gặp được nhau không phải là ngẫu nhiên thế nên iu nhá <3',
+    question:'Toai biết ngay thế nào cũng chọn không mà , 100 hạt mưa không hạt nào rơi nhầm chổ rơi 2 lần không phải là ngẫu nhiên ,Trả Lời Mình Hợp Nhau Phải Hông , Iu nhá<3',
     btnReply: 'Gửi cho ông anh ik <3',
     reply: 'oce cũng cũng yêuuu rất nhìu  <33333333',
     mess: 'Tôi biết mà iuuuuu bé nhìu  lém 😘😘',
-    messDesc: 'Tuần sau lên đón ik chơi nhé công chúa.',
+    messDesc: 'Mai lên đón ik chơi nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
